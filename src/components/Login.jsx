1c17b2f78cf1bd49ed986f890f9dd4ff1736d93a
emonShimoul <emonshimoul@gmail.com>
2025-02-10 23:16:21 +0600

    design login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,28 @@
 const Login = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-      <h2>Login</h2>
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col">
+        <div className="text-center lg:text-left">
+          <h1 className="text-3xl font-bold">Login Now!</h1>
+        </div>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <form onSubmit={handleLogin} className="card-body">
+            <fieldset className="fieldset">
+              <label className="fieldset-label">Email</label>
+              <input type="email" className="input" placeholder="Email" />
+              <label className="fieldset-label">Password</label>
+              <input type="password" className="input" placeholder="Password" />
+              <div>
+                <a className="link link-hover">Forgot password?</a>
+              </div>
+              <button className="btn btn-neutral mt-4">Login</button>
+            </fieldset>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
